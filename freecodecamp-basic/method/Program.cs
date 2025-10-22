971445ad46489c5d4ac72e7e53f01c119929ecee
Ibrohim Aqimuddin <ibrohimaqimuddin@gmail.com>
2025-10-22 16:40:35 +0700

    Simple Game

---
+++ freecodecamp-basic/method/Program.cs
@@ -0,0 +1,65 @@
+﻿string[] address;
+bool validLength = false;
+bool validZeroes = false;
+bool validRange = false;
+
+Console.WriteLine("Enter number of IPv4 addresses to validate:");
+int arrayLength = int.Parse(Console.ReadLine());
+string[] ipv4Input = new string[arrayLength];
+
+Console.WriteLine("Enter the IPv4 addresses:");
+for (int i = 0; i < arrayLength; i++)
+{
+    ipv4Input[i] = Console.ReadLine();
+}
+
+foreach (string ip in ipv4Input)
+{
+    address = ip.Split(".", StringSplitOptions.RemoveEmptyEntries);
+
+    ValidateLength();
+    ValidateZeroes();
+    ValidateRange();
+
+    if (validLength && validZeroes && validRange)
+    {
+        Console.WriteLine($"{ip} is a valid IPv4 address");
+    }
+    else
+    {
+        Console.WriteLine($"{ip} is an invalid IPv4 address");
+    }
+}
+
+void ValidateLength() 
+{
+    validLength = address.Length == 4;
+};
+
+void ValidateZeroes() 
+{
+    foreach (string number in address) 
+    {
+        if (number.Length > 1 && number.StartsWith("0")) 
+        {
+            validZeroes = false;
+            return;
+        }
+    }
+
+    validZeroes = true;
+}
+
+void ValidateRange() 
+{
+    foreach (string number in address) 
+    {
+        int value = int.Parse(number);
+        if (value < 0 || value > 255) 
+        {
+            validRange = false;
+            return;
+        }
+    }
+    validRange = true;
+}
